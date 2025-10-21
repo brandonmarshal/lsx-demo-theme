154351f0c42ae53a8625fc848a02ed6189f5cb20
Brandonma21 <Brandonmarshall101@gmail.com>
2025-10-21 21:48:15 +0200

    Added new colour palettes and font pairings. Also Created new styles with those colour palettes.

---
+++ patterns/contact-form-bfa.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: contact form bfa
+ * Slug: lsx-demo-theme/contact-form-bfa
+ * Categories: Contact
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"20px","bottom":"20px","left":"20px","right":"20px"},"margin":{"top":"0px","bottom":"0px"}},"border":{"radius":"20px"}},"backgroundColor":"surface","layout":{"type":"default"}} -->
+<div class="wp-block-group has-surface-background-color has-background" style="border-radius:20px;margin-top:0px;margin-bottom:0px;padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"><!-- wp:gravityforms/form {"formId":"1","description":false,"inputBorderRadius":"4","inputColor":"#111111","inputPrimaryColor":"#E58F5C","labelFontSize":"16","labelColor":"#f6ac4e","descriptionFontSize":"14","descriptionColor":"#D9DBD6","buttonPrimaryBackgroundColor":"#879746"} /--></div>
+<!-- /wp:group -->
